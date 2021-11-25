a6c4de93006d970a5e6d43dd501ede44fbffc598
devmateusramos <devmateusramos@gmail.com>
2021-11-25 12:40:41 -0300

    socketio ok

---
+++ src/index.js
@@ -0,0 +1,31 @@
+import express from 'express';
+import http from 'http';
+
+import socketio from 'socket.io';
+
+const app = express();
+const server = http.Server(app);
+const io = socketio(server);
+
+app.use(express.static(__dirname + '/public'));
+
+io.on('connect', (socket) => {
+  io.to(socket.id).emit({
+    status: true,
+    message: 'conexão estabelecida com o servidor',
+  });
+
+  socket.on('teste', (res) => {
+    console.log('mensagem recebida nesse momento:', res);
+
+    io.to(socket.id).emit(res);
+  });
+});
+
+app.get('/', (req, res) => {
+  res.render('index.html');
+});
+
+server.listen(3333, () => {
+  console.log('Servidor iniciado porta', 3333);
+});
